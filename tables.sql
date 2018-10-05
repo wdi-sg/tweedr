b992de71a74a4acc402c5c6c8e382f0e9ee60895
chanleyou <chanleyou@gmail.com>
2018-10-05 16:04:21 +0800

    added timestamp default now to tweeds table

--- tables.sql
+++ tables.sql
@@ -1,21 +1,22 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS tweeds;
-DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS follows;
 
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
 	password TEXT
-};
+);
 
-CREATE TABLE IF NOT EXISTS tweeds {
+CREATE TABLE IF NOT EXISTS tweeds (
 	id SERIAL PRIMARY KEY,
 	content TEXT,
-	user_id INTEGER
-};
+	user_id INTEGER,
+	created_at TIMESTAMPTZ DEFAULT now ()
+);
 
-CREATE TABLE IF NOT EXISTS follows {
+CREATE TABLE IF NOT EXISTS follows (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER,
 	follower_id INTEGER
-};
+);
