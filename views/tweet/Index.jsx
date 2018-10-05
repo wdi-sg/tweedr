83f0acf58a02bff7eba640701b2c309bcd9cec8d
ChloeLiang <nliangxin@gmail.com>
2018-10-05 19:23:48 +0800

    Add style to tweets

--- views/tweet/Index.jsx
+++ views/tweet/Index.jsx
@@ -1,19 +1,27 @@
 const React = require('react');
+const Layout = require('../layout/Layout');
 
 class Index extends React.Component {
   render() {
     const tweets = this.props.tweets.map(tweet => {
       return (
-        <div key={tweet.id}>
-          <p>{tweet.tweet}</p>
+        <div key={tweet.id} className="card">
+          <div className="card-body">
+            <h5 className="card-title">Username</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Date time</h6>
+            <p className="card-text">{tweet.tweet}</p>
+          </div>
         </div>
       );
     });
 
     return (
-      <div>
-        {tweets}
-      </div>
+      <Layout>
+        <div className="container">
+          {tweets}
+        </div>
+      </Layout>
+
     );
   }
 }
