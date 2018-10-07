f9aebdbd45b2345dbf36eb340792123f3876356b
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-07 22:44:19 +0800

    Style all forms

--- views/tweeds/NewTweed.jsx
+++ views/tweeds/NewTweed.jsx
@@ -5,12 +5,21 @@ class NewUser extends React.Component {
   render() {
     return (
       <DefaultLayout>
-        <form className="tweed-form" method="POST" action="/tweeds">
-          <div className="user-attribute">
-            Tweed
-            <input name="content" type="text" />
+        <form className="mt-5" method="POST" action="/tweeds">
+          <div className="form-group row">
+            <label htmlFor="name" className="col-sm-12 col-form-label">
+              Tweed Away!
+            </label>
+            <div className="col-sm-10">
+              <input
+                name="content"
+                className="form-control"
+                type="text"
+                required
+              />
+            </div>
           </div>
-          <input name="submit" type="submit" />
+          <input type="submit" className="btn btn-primary" value="Submit" />
         </form>
       </DefaultLayout>
     );
--- views/user/NewUser.jsx
+++ views/user/NewUser.jsx
@@ -5,16 +5,34 @@ class NewUser extends React.Component {
   render() {
     return (
       <DefaultLayout>
-        <form className="user-form" method="POST" action="/users">
-          <div className="user-attribute">
-            name
-            <input name="name" type="text" />
+        <form className="mt-5" method="POST" action="/users">
+          <div className="form-group row">
+            <label htmlFor="name" className="col-sm-12 col-form-label">
+              Username
+            </label>
+            <div className="col-sm-10">
+              <input
+                name="name"
+                className="form-control"
+                type="text"
+                required
+              />
+            </div>
           </div>
-          <div className="user-attribute">
-            password:
-            <input name="password" type="text" />
+          <div className="form-group row">
+            <label htmlFor="name" className="col-sm-12 col-form-label">
+              Password
+            </label>
+            <div className="col-sm-10">
+              <input
+                name="password"
+                className="form-control"
+                type="password"
+                require
+              />
+            </div>
           </div>
-          <input name="submit" type="submit" />
+          <input type="submit" className="btn btn-primary" value="Submit" />
         </form>
       </DefaultLayout>
     );
