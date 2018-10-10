3ba41d11295fbf8a41ceaaa6d5b133e7b0b0b1a5
xnithunx <nithun.nanda@gmail.com>
2018-10-10 10:12:04 +0800

    updated

--- models/user.js
+++ models/user.js
@@ -1,32 +1,46 @@
 var sha256 = require('js-sha256');
 
+ //NO RENDERING, JUST DRAFTING OF FUNCTIONS
 
-/**
- * ===========================================
- * Export model functions as a module
- * ===========================================
- */
-module.exports = (dbPoolInstance) => {
-    const create = (user, callback) => {
-      // run user input password through bcrypt to obtain hashed password
+ /**
+  * ===========================================
+  * Export model functions as a module
+  * ===========================================
+  */
+ module.exports = (dbPoolInstance) => {
+     const create = (user, callback) => {
+       // run user input password through bcrypt to obtain hashed password
 
-      var hashedValue = sha256(user.password);
+       var hashedValue = sha256(user.password);
 
-      // set up query
-      const queryString = 'INSERT INTO users (name, password) VALUES ($1, $3)';
-      const values = [
-        user.name,
-        hashedValue
-      ];
+       // set up query
+     const queryString = 'INSERT INTO users (name, password) VALUES ($1, $2)';
+       const values = [user.name,hashedValue];
 
-      // execute query
-      dbPoolInstance.query(queryString, values, (error, queryResult) => {
-        // invoke callback function with results after query has executed
-        callback(error, queryResult);
-      });
-    };
+       // execute query
+       dbPoolInstance.query(queryString, values, (error, queryResult) => {
+ @@ -30,3 +28,28 @@ module.exports = (dbPoolInstance) => {
+       create
+     };
+ };
 
-    return {
-      create
-    };
-};
+ module.exports = (dbPoolInstance) => {
+     const newForm = (user, callback) => {
+
+       // set up query
+       const queryString = 'INSERT INTO user (name, password) VALUES ($1, $2)';
+       const values = [user.name,hashedValue];
+
+       // execute query
+       dbPoolInstance.query(queryString, values, (error, queryResult) => {
+         // invoke callback function with results after query has executed
+         callback(error, queryResult);
+       });
+     };
+
+     return {
+       newForm
+     };
+ };
+
+
