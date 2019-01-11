0808f5d3b33ccc069a2a72b927b23e12eeb74646
akira <akira@akiras-MacBook-Pro.local>
2019-01-11 13:36:46 +0800

    edits

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
-const db = require('./db');
+
+const pg = require('pg');
 
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
+
+const configs = {
+  user: 'akira',
+  host: '127.0.0.1',
+  database: 'testdb',
+  port: 5432,
+};
+
+const pool = new pg.Pool(configs);
+
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
 
 // Init express app
 const app = express();
@@ -31,9 +45,6 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-// Import routes to match incoming requests
-require('./routes')(app, db);
-
 // Root GET request (it doesn't belong in any controller file)
 app.get('/', (request, response) => {
   response.send('Welcome To Tweedr.');
@@ -44,15 +55,16 @@ app.get('/', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
+const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-// Run clean up actions when server shuts down
-server.on('close', () => {
-  console.log('Closed express server');
+let onClose = function(){
 
-  db.pool.end(() => {
-    console.log('Shut down db connection pool');
-  });
-});
+  server.close(() => {
+    console.log('Process terminated')
+    pool.end( () => console.log('Shut down db connection pool'));
+  })
+};
+
+process.on('SIGTERM', onClose);
+process.on('SIGINT', onClose);
