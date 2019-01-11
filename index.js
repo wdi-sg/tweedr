d2973ea532c2fc72595c001b73ecf4450b7f3b1e
rchuaml <ronchuaml@gmail.com>
2019-01-11 16:38:32 +0800

    added tables and seed and installed packages and boilerplate

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+console.log("starting up!!");
+
 const express = require('express');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
-
 const pg = require('pg');
+
+// Initialise postgres client
+const configs = {
+    user: 'ronniechua',
+    host: '127.0.0.1',
+    database: 'tweedr',
+    port: 5432,
+};
+
+const pool = new pg.Pool(configs);
+
+pool.on('error', function(err) {
+    console.log('idle client error', err.message, err.stack);
+});
 
 /**
  * ===================================
@@ -10,34 +25,25 @@ const pg = require('pg');
  * ===================================
  */
 
-const configs = {
-  user: 'akira',
-  host: '127.0.0.1',
-  database: 'testdb',
-  port: 5432,
-};
-
-const pool = new pg.Pool(configs);
-
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
-
 // Init express app
 const app = express();
 
-// Set up middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + '/public'));
+
 app.use(methodOverride('_method'));
 app.use(cookieParser());
-app.use(express.urlencoded({
-  extended: true
-}));
+
 
 // Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', reactEngine);
+
 
 /**
  * ===================================
---
+++ seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO users(name, photo_url, username, password) VALUES('Yeah Yeah Yeahs', 'http://www.athousandguitars.com/wp-content/uploads/2013/04/yeah-yeah-yeahs.jpg', 'yeahyeah', 'yeahyeah');
+INSERT INTO users(name, photo_url, username, password) VALUES('Nosaj Thing', 'http://wertn.com/wp-content/uploads/2012/04/Nosaj-Thing_Mondrian_CL_High-3487.jpg', 'nosaj', 'nosaj');
+INSERT INTO users(name, photo_url, username, password) VALUES('Norah Jones', 'http://entertainmentrealm.files.wordpress.com/2012/05/norahjones1.jpg', 'norah','norah');
+INSERT INTO users(name, photo_url, username, password) VALUES('Lykke Li', 'http://www.chartattack.com/wp-content/uploads/2012/07/lykke-li-newmain1-photo-by-daniel-jackson.jpg', 'lykke', 'lykke');
+INSERT INTO users(name, photo_url, username, password) VALUES('Kendrick Lamar', 'http://www.xxlmag.com/wp-content/uploads/2013/06/kendricklamar_001-1600.jpg', 'kendrick', 'kendrick');
+INSERT INTO users(name, photo_url, username, password) VALUES('Ronnie Coleman', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Ronnie_Coleman_8_x_Mr_Olympia_-_2009_-_5.png/245px-Ronnie_Coleman_8_x_Mr_Olympia_-_2009_-_5.png', 'ronnie', 'ronnie123');
+INSERT INTO users(name, photo_url, username, password) VALUES('seany', 'https://media.giphy.com/media/EKUvB9uFnm2Xe/giphy.gif', 'seany', 'seany');
+
+
+
+INSERT INTO tweets(content, author_id) VALUES('People die when they are killed!', '1');
+INSERT INTO tweets(content, author_id) VALUES('NANI', '1');
+INSERT INTO tweets(content, author_id) VALUES('Sky is so high', '1');
+INSERT INTO tweets(content, author_id) VALUES('Goodday', '2');
+INSERT INTO tweets(content, author_id) VALUES('Have a nice day', '2');
+INSERT INTO tweets(content, author_id) VALUES('Hi there', '3');
+INSERT INTO tweets(content, author_id) VALUES('lol', '3');
+INSERT INTO tweets(content, author_id) VALUES('GOGO POWER RANGER', '3');
+INSERT INTO tweets(content, author_id) VALUES('YOLO', '3');
+INSERT INTO tweets(content, author_id) VALUES('Nothing here move along', '4');
+INSERT INTO tweets(content, author_id) VALUES('MonkaS', '5');
+INSERT INTO tweets(content, author_id) VALUES('MonkaGiga', '5');
+INSERT INTO tweets(content, author_id) VALUES('Frog', '5');
+INSERT INTO tweets(content, author_id) VALUES('Sweet Gains', '6');
+INSERT INTO tweets(content, author_id) VALUES('Im So BUFF!!', '6');
+INSERT INTO tweets(content, author_id) VALUES('REEEEEEE', '7');
+INSERT INTO tweets(content, author_id) VALUES('Me like cookie!', '7');
