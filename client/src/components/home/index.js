f361aa687fc2a6fb66ab94306b9c925e94db1660
kauvia <mkhw1989@gmail.com>
2019-01-13 23:10:13 +0800

    added logout functionality

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -6,10 +6,13 @@ class Home extends Component {
     super(props);
     this.state = {
       tweedContent: "",
-      tweeds: []
+      tweeds: [],
+      redirect: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+
   }
   componentDidMount() {
     this.getTweeds();
@@ -23,7 +26,13 @@ class Home extends Component {
   handleSubmit(e) {
     e.preventDefault();
     console.log(this.state);
-      this.submitTweeds();
+    this.submitTweeds();
+  }
+  handleLogout() {
+    console.log("logging out");
+    localStorage.clear();
+    console.log(this.state.redirect)
+    this.setState({ redirect: true });
   }
   getTweeds() {
     fetch("/home", {
@@ -57,42 +66,57 @@ class Home extends Component {
     );
   }
   render() {
-    if (localStorage.getItem("isLoggedIn")) {
-      return (
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">Tweedr</div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
-                <div className="form-group">
-                  <input
-                    name="tweedContent"
-                    type="text"
-                    className="form-control"
-                    id="inputTweed"
-                    placeholder="What's on your mind?"
-                  />
-                  <button
-                    type="submit"
-                    value="Submit"
-                    className="btn btn-primary"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-            {this.state.tweeds.map((val,idx)=>{
-              console.log(val)
-              return (<div className="row" key={idx}>{val.user}:{val.content} at {val.updatedAt}</div>)
-            })}
-        </div>
-      );
-    } else {
+    if (this.state.redirect) {
       return <Redirect to="/" />;
+    } else {
+      if (localStorage.getItem("isLoggedIn")) {
+        return (
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col">Tweedr</div>
+              <button
+                type="logout"
+                onClick={this.handleLogout}
+                className="btn btn-primary"
+              >
+                Logout
+              </button>
+            </div>
+            <div className="row">
+              <div className="col">
+                <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+                  <div className="form-group">
+                    <input
+                      name="tweedContent"
+                      type="text"
+                      className="form-control"
+                      id="inputTweed"
+                      placeholder="What's on your mind?"
+                    />
+                    <button
+                      type="submit"
+                      value="Submit"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            {this.state.tweeds.map((val, idx) => {
+              console.log(val);
+              return (
+                <div className="row" key={idx}>
+                  {val.user}:{val.content} at {val.updatedAt}
+                </div>
+              );
+            })}
+          </div>
+        );
+      } else {
+        return <Redirect to="/" />;
+      }
     }
   }
 }
