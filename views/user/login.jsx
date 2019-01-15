4cee416812a8467bce92633e55c20cf5ae7c077d
jumpingrock <kennethyeongky@gmail.com>
2019-01-15 09:57:21 +0800

    first commit

---
+++ views/user/login.jsx
@@ -0,0 +1,25 @@
+var React = require("react");
+
+class NewUser extends React.Component {
+  render() {
+    return (
+      <html>
+        <head />
+        <body>
+          <h2>Welcome to Tweedr</h2>
+          <form className="user-form" method="POST" action="/users">
+            <div className="user-attribute">
+              name<input name="name" type="text" />
+            </div>
+            <div className="user-attribute">
+              password:<input name="password" type="text" />
+            </div>
+            <input name="submit" id="loginsubmit" type="submit" />
+          </form>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = NewUser;
